bcbc7415ea454c4e54acfcad7164e49acc58ea56
WeiYuanStudio <weiyuanstudio@gmail.com>
2019-04-01 21:42:51 -0400

    review_ex05_IV完毕

--- ex05/IV/CommissionWorker.java
+++ ex05/IV/CommissionWorker.java
@@ -9,11 +9,32 @@ package ex05.IV;
  * @since JDK 11.0.2
  */
 public final class CommissionWorker extends Employee {
-    private double fixed_salary = 100; //固定工资
-    private double floating_wage = 200; //浮动工资
+    private double fixedSalary; //固定工资
+    private double floatingWage; //浮动工资
+
+    public CommissionWorker(double fixedSalary, double floatingWage) {
+        this.fixedSalary = fixedSalary;
+        this.floatingWage = floatingWage;
+    }
 
     @Override
     double Earnings() {
-        return fixed_salary + floating_wage;
+        return fixedSalary + floatingWage;
+    }
+
+    public double getFixedSalary() {
+        return fixedSalary;
+    }
+
+    public void setFixedSalary(double fixedSalary) {
+        this.fixedSalary = fixedSalary;
+    }
+
+    public double getFloatingWage() {
+        return floatingWage;
+    }
+
+    public void setFloatingWage(double floatingWage) {
+        this.floatingWage = floatingWage;
     }
 }
